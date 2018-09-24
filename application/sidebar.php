38ddbc71235464053877fe1b825e02d162991b3e
Fahad <ifahaduddin@gmail.com>
2018-09-24 23:37:15 +0500

    first commit

---
+++ application/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fahad Uddin
+ * Date: 3/11/2018
+ * Time: 10:12 PM
+ */
+//echo "sidebar";
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fahad Uddin
+ * Date: 3/11/2018
+ * Time: 10:10 PM
+ */
+require_once("application/header.php");
+require_once("application/content/content-page.php");
+require_once("application/sidebar.php");
+require_once("application/footer.php");
